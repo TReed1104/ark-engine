68255b32a78dac2cecb6ffa639a5be42872357d3
Tom Reed <T.Reed1104@gmail.com>
2017-03-02 19:38:10 +0000

    Implemented a string split function.

---
+++ Source/Utilities.hpp
@@ -0,0 +1,37 @@
+#ifndef ARKENGINE_UTILITYIES_HPP_
+#define ARKENGINE_UTILITYIES_HPP_
+#include <string>
+#include <vector>
+
+class Utilities
+{
+public:
+	Utilities() {}
+	~Utilities() {}
+
+	static std::vector<std::string> StringSplit(const std::string& stringToSplit, const char& splitToken) {
+		std::vector<std::string> splitString;
+
+		std::string currentSplit = "";
+		int sizeOfStringArray = stringToSplit.size();
+		for (unsigned int i = 0; i < sizeOfStringArray; i++) {
+			if (stringToSplit.at(i) == splitToken) {
+				splitString.push_back(currentSplit);
+				currentSplit = "";
+			}
+			else {
+				currentSplit += stringToSplit.at(i);
+			}
+			if (i == sizeOfStringArray - 1) {
+				splitString.push_back(currentSplit);
+			}
+		}
+
+		return splitString;
+	}
+
+private:
+
+};
+
+#endif
