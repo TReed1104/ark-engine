d71d6b5713fa201a6b4f294ddd55dfa00b49ecde
Tom Reed <T.Reed1104@gmail.com>
2017-03-15 18:55:24 +0000

    Started adding a structure to the item test script

---
+++ content/scripts/item_test.lua
@@ -0,0 +1,13 @@
+item = {
+	-- Items identifiers
+	name = "TEST_ITEM",
+	type = "Consumable",
+
+	-- Item Durability
+	durability = {
+		is_destructible = true,
+		current = 100,
+		max = 100,
+	},
+
+},
