87929d1dac6d32aa8613526781541cace9379e32
Tom Reed <T.Reed1104@gmail.com>
2017-03-16 14:53:58 +0000

    Added a default texture for if texture assignment fails and moved the player script into the entity script folder

---
+++ Content/scripts/entities/player.lua
@@ -0,0 +1,10 @@
+player = {
+	-- Entity identifiers
+	name = "Player",
+	-- Texture
+	texture = "player.png",
+	-- Position
+	position = { x = 48, y = 64, z = 0,},
+	-- Stats
+	health = 10.0,
+}
--- content/scripts/player.lua
+++
@@ -1,6 +0,0 @@
-player = {
-	-- Player Position
-	position = { x = 0, y = 0, },
-	-- Player Stats
-	health = 10,
-}
