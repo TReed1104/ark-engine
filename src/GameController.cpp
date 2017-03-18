ac1644940730a36e799dc8863b9a2132d302bc14
Tom Reed <T.Reed1104@gmail.com>
2017-03-18 17:56:19 +0000

    Added dictionaries to handle the controllers input states

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -2,6 +2,27 @@
 
 GameController::GameController(SDL_GameController* sdlHook) {
 	gameController = sdlHook;
+
+	// Setup the button dictionary
+	buttonInputs["Button A"] = false;
+	buttonInputs["Button B"] = false;
+	buttonInputs["Button X"] = false;
+	buttonInputs["Button Y"] = false;
+	buttonInputs["Button Start"] = false;
+	buttonInputs["Button Back"] = false;
+	buttonInputs["Button Left Trigger"] = false;
+	buttonInputs["Button Left Bumper"] = false;
+	buttonInputs["Button Right Trigger"] = false;
+	buttonInputs["Button Right Bumper"] = false;
+	buttonInputs["Button DPad Up"] = false;
+	buttonInputs["Button DPad Down"] = false;
+	buttonInputs["Button DPad Left"] = false;
+	buttonInputs["Button DPad Right"] = false;
+
+	thumbStickAxis["Left Thumb X"] = 0.0f;
+	thumbStickAxis["Left Thumb Y"] = 0.0f;
+	thumbStickAxis["Right Thumb X"] = 0.0f;
+	thumbStickAxis["Right Thumb Y"] = 0.0f;
 }
 
 GameController::~GameController() {
--- src/GameController.h
+++ src/GameController.h
@@ -2,11 +2,14 @@
 #define ARKENGINE_GAMECONTROLLER_H_
 
 #include "SDL.h"
+#include <map>
 
 class GameController
 {
 public:
-
+	// Input dictionaries
+	std::map<std::string, bool> buttonInputs;
+	std::map<std::string, float> thumbStickAxis;
 
 	GameController(SDL_GameController* sdlHook);
 	~GameController();
