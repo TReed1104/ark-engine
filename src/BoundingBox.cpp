8100cfb7e62ca68f322fe9d4dd7b9a98fffae917
Tom Reed <T.Reed1104@gmail.com>
2017-03-21 23:24:13 +0000

    implemented the base of the BoundingBox class

--- src/BoundingBox.cpp
+++ src/BoundingBox.cpp
@@ -1,1 +1,22 @@
 #include "BoundingBox.h"
+#include "Engine.h"
+
+Engine* BoundingBox::Engine_Pointer;
+
+BoundingBox::BoundingBox(float x, float y, float width, float height) {
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
+
+BoundingBox::BoundingBox(glm::vec2 position, glm::vec2 dimensions) {
+	this->x = position.x;
+	this->y = position.y;
+	this->width = dimensions.x;
+	this->height = dimensions.y;
+}
+
+BoundingBox::~BoundingBox() {
+
+}
--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -1,4 +1,25 @@
 #ifndef ARKENGINE_BOUNDINGBOX_H_
 #define ARKENGINE_BOUNDINGBOX_H_
+#include "glm/glm.hpp"
+
+class Engine;
+
+class BoundingBox
+{
+public:
+	static Engine* Engine_Pointer;
+
+	float x;
+	float y;
+	float width;
+	float height;
+
+	BoundingBox(float x = 0.0f, float y = 0.0f, float width = 0.0f, float height = 0.0f);
+	BoundingBox(glm::vec2 position = glm::vec2(0.0f, 0.0f), glm::vec2 dimensions = glm::vec2(0.0f, 0.0f));
+	~BoundingBox();
+
+private:
+
+};
 
 #endif
