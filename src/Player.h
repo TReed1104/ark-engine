3feb0828bf403a0c33fd564e215c05f92e6a20e9
Tom Reed <T.Reed1104@gmail.com>
2017-03-21 23:09:41 +0000

    Added variables storing the keybinds for the four directions of movement

--- src/Player.h
+++ src/Player.h
@@ -1,10 +1,16 @@
 #ifndef ARKENGINE_PLAYER_H_
 #define ARKENGINE_PLAYER_H_
 #include "Entity.h"
+#include "Keyboard.h"
 
 class Player : public Entity
 {
 public:
+	Keyboard::Keys keybindMovementUp = Keyboard::w;
+	Keyboard::Keys keybindMovementDown = Keyboard::s;
+	Keyboard::Keys keybindMovementLeft = Keyboard::a;
+	Keyboard::Keys keybindMovementRight = Keyboard::d;
+
 	Player(const Model& model, const Texture& texture, const glm::vec3& position = glm::vec3(0.0f), const glm::vec2& sourceFrameSize = glm::vec2(16, 16));
 	~Player();
 	
