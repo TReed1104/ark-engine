c8050f4713f50215ea5f25d9c5aca751d3359f09
Tom Reed <T.Reed1104@gmail.com>
2017-03-21 23:36:41 +0000

    Changed the constructors for BoundingBox slightly as they were causing an error for both defaulting

--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -15,7 +15,7 @@ public:
 	float height;
 
 	BoundingBox(float x = 0.0f, float y = 0.0f, float width = 0.0f, float height = 0.0f);
-	BoundingBox(glm::vec2 position = glm::vec2(0.0f, 0.0f), glm::vec2 dimensions = glm::vec2(0.0f, 0.0f));
+	BoundingBox(glm::vec2 position, glm::vec2 dimensions);
 	~BoundingBox();
 
 	bool Intersect(BoundingBox otherBB);
