b452e31265c4daf5b3d13231fed19b44f569ce00
Tom Reed <T.Reed1104@gmail.com>
2017-03-22 21:35:42 +0000

    Added some test defaults to the key_bindings.lua

--- content/config/key_bindings.lua
+++ content/config/key_bindings.lua
@@ -1,17 +1,6 @@
-config = {
-	-- Tile Size
-	tile_size = { x = 16, y = 16, },
-
-	-- Window Size in Tiles
-	window_grid = { x = 16, y = 14, },
-
-	-- Window Scaler
-	window_scale = { x = 1.0, y = 1.0, },
-
-	-- Content Directory names
-	shaders_directory = "shaders",
-	levels_directory = "levels",
-	models_directory = "models",
-	textures_directory = "textures",
-	scripts_directory = "scripts",
+keybindings = {
+	up = 32,
+	down = 28,
+	left = 10,
+	right = 13,
 }
