a952d22f539394b33d21238bc63aadaf8e792a32
Tom Reed <T.Reed1104@gmail.com>
2017-04-01 16:44:43 +0100

    Amended the Entity and Player scripts to reflect the new data added to them

--- content/scripts/entities/entity_player.lua
+++ content/scripts/entities/entity_player.lua
@@ -1,10 +1,20 @@
-player = {
+entity = {
 	-- Entity identifiers
 	name = "Player",
 	-- Texture
 	texture = "player.png",
+	-- Source rectangle
+	source_rectangle_dimensions = { x = 16, y = 16, },
 	-- Position
-	position = { x = 48, y = 64, z = 0,},
+	position = { x = 48, y = 64, z = 0, },
+	-- Draw Offset
+	draw_offset = { x = 0, y = 0, },
+	-- Bounding box Dimensions
+	bounding_box_dimensions = { width = 10, height = 10, },
+	-- Bounding box offset
+	bounding_box_offset = { x = 3, y = 3, },
+	
 	-- Stats
 	health = 10.0,
+	movement_speed = 60.0,
 }
