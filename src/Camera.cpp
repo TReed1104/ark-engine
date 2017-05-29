44df168a63e047b545087511ddb410a61589c0b3
Tom Reed <T.Reed1104@gmail.com>
2017-05-29 19:40:41 +0100

    Amended the camera FollowObject() function to toggle between pan and centered

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -29,14 +29,30 @@ void Camera::Update(const float& deltaTime, GameObject& object) {
 }
 void Camera::FollowObject(const float& deltaTime, GameObject& object) {
 	if (&object != nullptr) {
-		centerPoint = object.position + glm::vec3((object.boundingBox.GetDimensions() / 2.0f), position.z);
-		glm::vec2 viewPortOffset = (viewPort / 2.0f);
-		position = centerPoint - glm::vec3(viewPortOffset, 0.0f);
-
-		//glm::vec3 targetPosition = centerPoint - glm::vec3(viewPortOffset, 0.0f);
-		//targetPosition.x = glm::clamp(targetPosition.x, 0.0f, (Engine_Pointer->levelRegister[Engine_Pointer->indexCurrentLevel]->pixelGridSize.x - (Engine_Pointer->windowGridSize * Engine_Pointer->tileSize).x));
-		//targetPosition.y = glm::clamp(targetPosition.y, 0.0f, (Engine_Pointer->levelRegister[Engine_Pointer->indexCurrentLevel]->pixelGridSize.y - (Engine_Pointer->windowGridSize * Engine_Pointer->tileSize).y));
-		//position += (targetPosition - position) * (1.0f * deltaTime);
+		bool isCameraCenter = true;
+		if (isCameraCenter) {
+			// Camera is centered on the object.
+			// Update the center point of the camera to be the center of the objects sprite.
+			centerPoint = object.position + glm::vec3((object.boundingBox.GetDimensions() / 2.0f), position.z);
+			// Update the camera position.
+			position = centerPoint - glm::vec3(viewPort / 2.0f, 0.0f);
+		}
+		else {
+			// Camera will pan towards the center of the object at the given speed.
+			// Update the center point of the camera to be the center of the objects sprite.
+			centerPoint = object.position + glm::vec3((object.boundingBox.GetDimensions() / 2.0f), position.z);
+			// Calculate the position we want to move the camera to
+			glm::vec3 targetPosition = centerPoint - glm::vec3(viewPort / 2.0f, 0.0f);
+			// Clamp the targetPosition to the bounds of the world as the camera's position already is.
+			targetPosition.x = glm::clamp(targetPosition.x, 0.0f, (Engine_Pointer->levelRegister[Engine_Pointer->indexCurrentLevel]->pixelGridSize.x - (Engine_Pointer->windowGridSize * Engine_Pointer->tileSize).x));
+			targetPosition.y = glm::clamp(targetPosition.y, 0.0f, (Engine_Pointer->levelRegister[Engine_Pointer->indexCurrentLevel]->pixelGridSize.y - (Engine_Pointer->windowGridSize * Engine_Pointer->tileSize).y));
+			// Calculate the difference between the targetposition and where the camera currently is
+			glm::vec3 cameraDelta = (targetPosition - position);
+			// Set the Speed the camera is to move at
+			float speed = 1.0f;
+			// Update the camera position.
+			position += cameraDelta * (speed * deltaTime);
+		}
 	}
 }
 void Camera::ManualControl(void) {
