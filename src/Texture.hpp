8731ae1bd2e8ff70cdefc68ff9dc4f9f93993108
Tom Reed <T.Reed1104@gmail.com>
2017-06-06 16:55:21 +0100

    Added variables tot eh texture class about each of its attributes

--- src/Texture.hpp
+++ src/Texture.hpp
@@ -8,10 +8,20 @@ class Texture {
 public:
 	std::string name;
 	GLuint id;
+	glm::ivec2 dimensionsInPixels;
+	glm::ivec2 dimensionsInTiles;
+	glm::ivec2 frameSize;
+	glm::ivec2 frameSizeBordered;
+	int numberOfFrames;
 
 	Texture(const std::string& name) {
 		this->name = name;
 		this->id = -1;
+		this->dimensionsInPixels = glm::ivec2(0, 0);
+		this->dimensionsInTiles = glm::ivec2(0, 0);
+		this->frameSize = glm::ivec2(0, 0);
+		this->frameSizeBordered = glm::ivec2(0, 0);
+		this->numberOfFrames = 0;
 	}
 	~Texture() {
 
