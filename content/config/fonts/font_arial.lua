79acc73866a0379f136c391aa761485bec6a154a
Tom Reed <T.Reed1104@gmail.com>
2017-07-07 11:31:05 +0100

    Amended the test font config

---
+++ content/config/fonts/font_arial.lua
@@ -0,0 +1,36 @@
+font = {
+    name = "arial",
+    texture = "arial.png",
+    model = "font_canvas.obj",
+    number_of_characters = "50",
+
+    characters = {
+        character_0   = { glyph = "a", source_frame_position = { x = 00, y = 00, }, },
+        character_1   = { glyph = "b", source_frame_position = { x = 01, y = 00, }, },
+        character_2   = { glyph = "c", source_frame_position = { x = 02, y = 00, }, },
+        character_3   = { glyph = "d", source_frame_position = { x = 03, y = 00, }, },
+        character_4   = { glyph = "e", source_frame_position = { x = 04, y = 00, }, },
+        character_5   = { glyph = "f", source_frame_position = { x = 05, y = 00, }, },
+        character_6   = { glyph = "g", source_frame_position = { x = 06, y = 00, }, },
+        character_7   = { glyph = "h", source_frame_position = { x = 07, y = 00, }, },
+        character_8   = { glyph = "i", source_frame_position = { x = 08, y = 00, }, },
+        character_9   = { glyph = "j", source_frame_position = { x = 09, y = 00, }, },
+        character_10  = { glyph = "k", source_frame_position = { x = 00, y = 01, }, },
+        character_11  = { glyph = "l", source_frame_position = { x = 01, y = 01, }, },
+        character_12  = { glyph = "m", source_frame_position = { x = 02, y = 01, }, },
+        character_13  = { glyph = "n", source_frame_position = { x = 03, y = 01, }, },
+        character_14  = { glyph = "o", source_frame_position = { x = 04, y = 01, }, },
+        character_15  = { glyph = "p", source_frame_position = { x = 05, y = 01, }, },
+        character_16  = { glyph = "q", source_frame_position = { x = 06, y = 01, }, },
+        character_17  = { glyph = "r", source_frame_position = { x = 07, y = 01, }, },
+        character_18  = { glyph = "s", source_frame_position = { x = 08, y = 01, }, },
+        character_19  = { glyph = "t", source_frame_position = { x = 09, y = 01, }, },
+        character_20  = { glyph = "u", source_frame_position = { x = 00, y = 02, }, },
+        character_21  = { glyph = "v", source_frame_position = { x = 01, y = 02, }, },
+        character_22  = { glyph = "w", source_frame_position = { x = 02, y = 02, }, },
+        character_23  = { glyph = "x", source_frame_position = { x = 03, y = 02, }, },
+        character_24  = { glyph = "y", source_frame_position = { x = 04, y = 02, }, },
+        character_25  = { glyph = "z", source_frame_position = { x = 05, y = 02, }, },
+    }
+
+}
