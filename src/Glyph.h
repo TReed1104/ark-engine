28075e375a08b724a0e25747797ac00f9d73120d
Tom Reed <T.Reed1104@gmail.com>
2017-07-21 13:17:04 +0100

    Added a comment about the structure of the Glyph class.

--- src/Glyph.h
+++ src/Glyph.h
@@ -25,6 +25,7 @@ public:
 	Texture texture;
 	glm::vec4 colour;
 
+	// Due to the way SDL_TTF outputs its glyph metrics, the Glyph class has to use public fields and a blank constructor.
 	Glyph(const char& character = NULL);
 	~Glyph();
 
