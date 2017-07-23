95d6880cbf0e76d00ff54596e7c1b1cfd117b72e
Tom Reed <T.Reed1104@gmail.com>
2017-07-23 13:40:11 +0100

    Amended the Engine utilities to return -1, an invalid value, temporarily whilst they are being implemented

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -615,26 +615,26 @@ glm::ivec2 Engine::ConvertToGridPosition(const glm::vec2 & position) {
 	return glm::vec2((int)(position.x / this->tileSize.x), (int)(position.y / this->tileSize.y));
 }
 int Engine::GetIndexOfShader(const std::string & shaderName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfModel(const std::string & modelName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfTexture(const std::string & textureName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfFont(const std::string & fontName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfRenderableText(const std::string & renderableTextName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfTileset(const std::string & tilesetName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfEntity(const std::string & entityName) {
-	return 0;
+	return -1;
 }
 int Engine::GetIndexOfLevel(const std::string & levelName) {
-	return 0;
+	return -1;
 }
