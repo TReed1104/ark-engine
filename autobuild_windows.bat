eccecf8a0d23e03871c518e26dc9632d0592435b
Tom Reed <t.reed1104@gmail.com>
2018-09-28 22:23:34 +0100

    amended the script to set the build type fixing the issues caused by assimp

--- autobuild_windows.bat
+++ autobuild_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir build
 cd build
-conan install .. --build missing
+conan install .. -s build_type=Debug -s compiler="Visual Studio" -s compiler.runtime=MDd --build=missing
 cmake .. -G "Visual Studio 14 2015 Win64"
 cmake --build . --config Debug
 cd ..
