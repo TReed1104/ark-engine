100b0f23661b50d85ddfa88bbdae22f8bf61ee15
Tom Reed <t.reed1104@gmail.com>
2018-10-09 19:15:31 +0100

    Added a build script for VS2017 compiler

---
+++ build_vs17.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir build
+cd build
+conan install .. -s build_type=Debug -s compiler="Visual Studio" -s compiler.runtime=MDd --build=missing
+cmake .. -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Debug
+cd ..
