dd6571e97c7a22407a6f9c9842de87345863fc3e
Tom Reed <t.reed1104@gmail.com>
2018-12-23 12:27:58 +0000

    Added the starts of getting the glyph data from our font face

--- src/Font.cpp
+++ src/Font.cpp
@@ -1,10 +1,18 @@
 #include "Font.h"
 Font::Font(const FT_Library& library, const std::string& fontName) {
 	if (FT_New_Face(library, fontName.c_str(), 0, &freeTypeFace)) {
+		// Face failed to load, feedback to the engine for it to close
 		isLoaded = false;
 	}
 	else {
-		isLoaded = true;	
+		// Face has been created, now lets split it into our glyphs
+		
+		name = freeTypeFace->family_name;
+		style = freeTypeFace->style_name;
+		numberOfGlyphs = freeTypeFace->num_glyphs;
+
+
+		isLoaded = true; // We've loaded the font, feedback for the engine to continue loading
 	}
 }
 
--- src/Font.h
+++ src/Font.h
@@ -1,6 +1,7 @@
 #ifndef ARKENGINE_FONT_H_
 #define ARKENGINE_FONT_H_
 
+#include <iostream>
 #include <string>
 #include <ft2build.h>
 #include FT_FREETYPE_H
@@ -9,6 +10,9 @@ class Font
 {
 public:
 	bool isLoaded = false;
+	std::string name = "";
+	std::string style = "";
+	int numberOfGlyphs = 0;
 
 	Font(const FT_Library& library, const std::string& fontName);
 	~Font();
