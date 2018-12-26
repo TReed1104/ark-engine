14f77cb5e6e523a6374b7a76d9216a26eea7ef7e
Tom Reed <t.reed1104@gmail.com>
2018-12-26 17:37:55 +0000

    Removed the old Controller input code

--- src/Player.cpp
+++ src/Player.cpp
@@ -32,50 +32,5 @@ void Player::HandleInputKeyboard(void) {
 	}
 }
 void Player::HandleInputGameController(void) {
-	// Reset the movement direction
-	movementDirection = Player::NotSet;
-
-	// Get the Thumbstick
-	glm::vec2 leftThumbAxis = Engine_Pointer->deviceGameControllerRegister[Engine_Pointer->indexOfPlayerController]->GetThumbStickState(GameController::ThumbStickLeft);
-
-	if ((leftThumbAxis.x < -Engine_Pointer->thumbStickDeadZone) || Engine_Pointer->deviceKeyboard->GetKeyState(Engine_Pointer->keybindMovementLeft)) {
-		// Left movement
-		movementDirection = Directions::Left;
-		spriteDirection = Directions::Left;
-	}
-	else if ((leftThumbAxis.x > Engine_Pointer->thumbStickDeadZone) || Engine_Pointer->deviceKeyboard->GetKeyState(Engine_Pointer->keybindMovementRight)) {
-		// Right movement
-		movementDirection = Directions::Right;
-		spriteDirection = Directions::Right;
-	}
-	if ((leftThumbAxis.y < -Engine_Pointer->thumbStickDeadZone) || Engine_Pointer->deviceKeyboard->GetKeyState(Engine_Pointer->keybindMovementUp)) {
-		// Up movement
-		if (movementDirection == Directions::Left) {
-			movementDirection = Directions::UpLeft;
-			spriteDirection = Directions::Left;
-		}
-		else if (movementDirection == Directions::Right) {
-			movementDirection = Directions::UpRight;
-			spriteDirection = Directions::Right;
-		}
-		else {
-			movementDirection = Directions::Up;
-			spriteDirection = Directions::Up;
-		}
-	}
-	else if ((leftThumbAxis.y > Engine_Pointer->thumbStickDeadZone) || Engine_Pointer->deviceKeyboard->GetKeyState(Engine_Pointer->keybindMovementDown)) {
-		// Down movement
-		if (movementDirection == Directions::Left) {
-			movementDirection = Directions::DownLeft;
-			spriteDirection = Directions::Left;
-		}
-		else if (movementDirection == Directions::Right) {
-			movementDirection = Directions::DownRight;
-			spriteDirection = Directions::Right;
-		}
-		else {
-			movementDirection = Directions::Down;
-			spriteDirection = Directions::Down;
-		}
-	}
+	// TODO: Implement Controller support when Keyboard input is finished
 }
