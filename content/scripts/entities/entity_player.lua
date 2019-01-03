c3f5202005e088485fa67f4736f1ea38939f7973
Tom Reed <t.reed1104@gmail.com>
2019-01-03 21:39:32 +0000

    Implemented a test level thats far wider in range than the last

--- content/scripts/entities/entity_player.lua
+++ content/scripts/entities/entity_player.lua
@@ -4,7 +4,7 @@ entity = {
 	-- Texture
 	texture = "entity_player.png",
 	-- Position
-	position = { x = 38, y = 20, z = 0, },
+	position = { x = 90, y = 30, z = 0, },
 	-- Draw Offset
 	draw_offset = { x = -24, y = -24, },
 	-- Bounding box Dimensions
