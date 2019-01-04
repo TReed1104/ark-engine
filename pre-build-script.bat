975239e1a7f4330471df766fbca5601f797e8c54
Tom Reed <t.reed1104@gmail.com>
2019-01-04 12:16:16 +0000

    Amended the pre build script to purge the old content directory first to prevent old files migrating

--- pre-build-script.bat
+++ pre-build-script.bat
@@ -1,3 +1,7 @@
-@echo off
-xcopy /q /i /e /y ..\content .\content
-xcopy /q /i /e /y ..\content .\bin\content
+@ECHO OFF
+ECHO Removing Old Content
+RMDIR /s /q .\content
+RMDIR /s /q .\bin\content
+ECHO Copying Content
+XCOPY /q /i /e /y ..\content .\content
+XCOPY /q /i /e /y ..\content .\bin\content
