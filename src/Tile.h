fd882d21271e3bffa53acd9bd27f619ba1f14476
Tom Reed <t.reed1104@gmail.com>
2019-01-11 11:55:24 +0000

    Added the variables for slopeOffset and slopeAngle

--- src/Tile.h
+++ src/Tile.h
@@ -8,6 +8,8 @@ public:
 	enum Type { Walkable, Solid, };
 	Type type;
 	bool isSlope;
+	glm::vec2 slopeOffset;	// x = left, y = right
+	float slopeAngle;
 
 	Tile(const Texture& texture, const int& tileType, const bool& isSlope = false, const glm::ivec2& sourceFramePosition = glm::ivec2(0), const glm::vec3& position = glm::vec3(0.0f), const BoundingBox& boundingBox = BoundingBox(glm::ivec2(0)), const glm::ivec2& boundingBoxOffset = glm::ivec2(0));
 	~Tile(void);
