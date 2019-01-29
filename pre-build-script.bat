a903bf966adc6c12d44a936d7bc2913f8ecbd895
Tom Reed <t.reed1104@gmail.com>
2019-01-29 21:25:07 +0000

    Amended the pre-build-script to not copy the content_json folder anymore as its been removed

--- pre-build-script.bat
+++ pre-build-script.bat
@@ -2,10 +2,6 @@
 ECHO Removing Old Content
 RMDIR /s /q .\content
 RMDIR /s /q .\bin\content
-RMDIR /s /q .\content_json
-RMDIR /s /q .\bin\content_json
 ECHO Copying Content
 XCOPY /q /i /e /y ..\content .\content
 XCOPY /q /i /e /y ..\content .\bin\content
-XCOPY /q /i /e /y ..\content_json .\content_json
-XCOPY /q /i /e /y ..\content_json .\bin\content_json
