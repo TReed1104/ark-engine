219d112816391e34590dc56ffdd4444a73c50f10
Tom Reed <t.reed1104@gmail.com>
2019-02-01 11:14:44 +0000

    Implemented the AddKeyBinding() funciton

--- src/Keybindings.cpp
+++ src/Keybindings.cpp
@@ -9,5 +9,14 @@ Keyboard::Keys KeybindingHandler::GetKeybind(const std::string & bindingName) {
 	return Keyboard::Keys();
 }
 bool KeybindingHandler::AddKeybinding(const Keybind& newBinding) {
-	return false;
+	// Check fi the binding exists
+	if (bindings.find(newBinding.id) == bindings.end()) {
+		// If it doesn't already exist, add it
+		bindings[newBinding.id] = newBinding;
+		return true;
+	}
+	else {
+		std::cout << ">>>> Error - Binding already exists" << std::endl;
+		return false;
+	}
 }
