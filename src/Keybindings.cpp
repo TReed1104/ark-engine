e21f3fab25f4b76dee42903a4a594a2872b55e16
Tom Reed <t.reed1104@gmail.com>
2019-02-01 11:27:43 +0000

    Changed the GetKeybind() function to use the friendly name, not the id

--- src/Keybindings.cpp
+++ src/Keybindings.cpp
@@ -7,13 +7,14 @@ KeybindingHandler::~KeybindingHandler() {
 
 Keyboard::Keys KeybindingHandler::GetKeybind(const std::string& bindingName) {
 	// TODO: Rewrite to use ASCII table value, no Enum index
-	return (Keyboard::Keys)bindings[bindingName].bindingValue;
+	return (Keyboard::Keys)bindings[friendlyNames[bindingName]].bindingValue;
 }
 bool KeybindingHandler::AddKeybinding(const Keybind& newBinding) {
 	// Check fi the binding exists
 	if (bindings.find(newBinding.id) == bindings.end()) {
 		// If it doesn't already exist, add it
 		bindings[newBinding.id] = newBinding;
+		friendlyNames[newBinding.friendlyName] = newBinding.id;	// Map the friendly name to the id
 		return true;
 	}
 	else {
