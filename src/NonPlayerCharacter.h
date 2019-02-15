98b479c6bfe95d7eb199e52bc929bc2ad3d64dc6
Tom Reed <t.reed1104@gmail.com>
2019-02-15 14:23:41 +0000

    Implemented base NPC class definition

--- src/NonPlayerCharacter.h
+++ src/NonPlayerCharacter.h
@@ -1,5 +1,16 @@
 #ifndef ARKENGINE_NONPLAYERCHARACYER_H_
 #define ARKENGINE_NONPLAYERCHARACYER_H_
 
+#include "Entity.h"
+
+
+class NonPlayerCharacter : public Entity {
+public:
+	NonPlayerCharacter(const std::string& filePath);
+	~NonPlayerCharacter(void);
+
+private:
+
+};
 
 #endif
