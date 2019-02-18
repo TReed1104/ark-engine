0bab043c456816bd1d92285ac9b32602564e515d
Tom Reed <t.reed1104@gmail.com>
2019-02-18 14:25:16 +0000

    Migrated to the conan recipe for SDL2_image-2.0.4 from SDL2_image-2.0.4

--- jeeves.bat
+++ jeeves.bat
@@ -20,5 +20,5 @@ conan remove -f libwebp/1.0.0
 conan remove -f nasm/2.13.01
 conan remove -f rapidjson/1.1.0
 conan remove -f sdl2/2.0.9
-conan remove -f sdl2_image/2.0.3
+conan remove -f sdl2_image/2.0.4
 conan remove -f zlib/1.2.11
