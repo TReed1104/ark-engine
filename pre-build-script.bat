9338270a17f6c36ee1d47d9fc581315c34dfb2d6
Tom Reed <t.reed1104@gmail.com>
2019-02-19 13:54:50 +0000

    Implemented the clearing and creation of the logging directories

--- pre-build-script.bat
+++ pre-build-script.bat
@@ -2,6 +2,13 @@
 ECHO Removing Old Content
 RMDIR /s /q .\content
 RMDIR /s /q .\bin\content
+
 ECHO Copying Content
 XCOPY /q /i /e /y ..\content .\content
 XCOPY /q /i /e /y ..\content .\bin\content
+
+ECHO Creating Logging Directories
+RMDIR .\logs
+RMDIR .\bin\logs
+MKDIR .\logs
+MKDIR .\bin\logs
