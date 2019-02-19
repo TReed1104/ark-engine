bebc787ca6337674d59345a7640284e5b70cf4f8
Tom Reed <t.reed1104@gmail.com>
2019-02-19 15:27:19 +0000

    Amended the flags of RMDIR in the pre-build-script

--- pre-build-script.bat
+++ pre-build-script.bat
@@ -8,7 +8,7 @@ XCOPY /q /i /e /y ..\content .\content
 XCOPY /q /i /e /y ..\content .\bin\content
 
 ECHO Creating Logging Directories
-RMDIR .\logs
-RMDIR .\bin\logs
+RMDIR /s /q .\logs
+RMDIR /s /q .\bin\logs
 MKDIR .\logs
 MKDIR .\bin\logs
