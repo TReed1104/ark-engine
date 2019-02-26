77449bd04cff3cf1cc0c59cba3ca0fe293739bab
Tom Reed <t.reed1104@gmail.com>
2019-02-26 09:35:32 +0000

    Removed two conditionals as they wouldn't ever be needed, due to them being in another condition that would catch it

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -345,18 +345,13 @@ void Engine::LoadOpenAL(void) {
 			engineDebugger.WriteLine(">>>>>> Default Audio context created");
 		}
 		else {
-			// TODO: Print our error message from OpenAL
-			if (alGetError() != AL_NO_ERROR) {
-				engineDebugger.WriteLine(">>>>>> ERROR!!!! - OpenAL Error: " + alGetError());
-			}
+			engineDebugger.WriteLine(">>>>>> ERROR!!!! - OpenAL Error: " + alGetError());
 			engineDebugger.WriteLine(">>>>>> ERROR!!!! - Default Audio context failed to load");
 		}
 	}
 	else {
 		// TODO: Print our error message from OpenAL
-		if (alGetError() != AL_NO_ERROR) {
-			engineDebugger.WriteLine(">>>>>> ERROR!!!! - OpenAL Error: " + alGetError());
-		}
+		engineDebugger.WriteLine(">>>>>> ERROR!!!! - OpenAL Error: " + alGetError());
 		engineDebugger.WriteLine(">>>>>> ERROR!!!! - Default Audio device failed to load");
 	}
 
