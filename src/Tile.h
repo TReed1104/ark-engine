994f20f8e6ebdb09b009751ada7f777c05203620
Tom Reed <t.reed1104@gmail.com>
2019-03-11 10:45:52 +0000

    Added the signature for a copy constructor

--- src/Tile.h
+++ src/Tile.h
@@ -11,6 +11,7 @@ public:
 	glm::vec2 slopeOffset;	// x = left, y = right
 	float slopeAngle;
 
+	Tile(Tile& otherTile);
 	Tile(const Texture& texture, const int& tileType, const glm::ivec2& sourceFramePosition = glm::ivec2(0), const glm::vec3& position = glm::vec3(0.0f), const BoundingBox& boundingBox = BoundingBox(glm::ivec2(0)), const glm::ivec2& boundingBoxOffset = glm::ivec2(0), const bool& isSlope = false, const glm::ivec2& slopeOffset = glm::vec2(0));
 	~Tile(void);
 
