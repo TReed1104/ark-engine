2fec4900b0cf459df54869bb941754003fedffd7
Tom Reed <t.reed1104@gmail.com>
2019-03-16 00:27:33 +0000

    Fixed the issue with the creation of the Glyph's texture, it wasn't using the new constructor and was false-ing a string

--- src/Glyph.cpp
+++ src/Glyph.cpp
@@ -1,7 +1,7 @@
 #include "Glyph.h"
 
 Glyph::Glyph(const char& character) {
-	this->texture = Texture(std::to_string(character), false);
+	this->texture = Texture(std::to_string(character));
 	this->character = character;
 	this->size = glm::vec2(0, 0);
 	this->advance = glm::vec2(0, 0);
