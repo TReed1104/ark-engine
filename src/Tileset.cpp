c9644db9790c4a56cca117a47ba7b920ee840ebf
Tom Reed <t.reed1104@gmail.com>
2019-03-17 00:17:24 +0000

    Added a further error check to the tileset load to prevent us using an invalid texture

--- src/Tileset.cpp
+++ src/Tileset.cpp
@@ -28,22 +28,28 @@ bool Tileset::Load() {
 		int indexOfTileSetTexture = -1;
 		std::string textureName = configFile->Get<std::string>("tileset.texture");
 		indexOfTileSetTexture = Engine_Pointer->GetIndexOfTexture(textureName);
-		size_t numberOfTiles = configFile->SizeOfObjectArray("tileset.tiles");
-		for (size_t i = 0; i < numberOfTiles; i++) {
-			int tileType = configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.type");
-			bool isSlope = configFile->Get<bool>("tileset.tiles." + std::to_string(i) + ".tile.slope.is slope");
-			glm::ivec2 slopeOffset = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.slope.slope offset.left"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.slope.slope offset.right"));
-			glm::ivec2 aabbOffset = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.offset.x"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.offset.y"));
-			glm::ivec2 aabbDimensions = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.dimensions.width"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.dimensions.height"));
-			glm::ivec2 sourceFramePosition = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.source frame.position.x"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.source frame.position.y"));
-			if (indexOfTileSetTexture != -1) {
-				tileList.push_back(Tile(Engine_Pointer->textureRegister[indexOfTileSetTexture], (Tile::Type)tileType, sourceFramePosition, glm::vec3(0.0f), BoundingBox(glm::ivec2(0), aabbDimensions), aabbOffset, isSlope, slopeOffset));
+		if (indexOfTileSetTexture != -1) {
+			size_t numberOfTiles = configFile->SizeOfObjectArray("tileset.tiles");
+			for (size_t i = 0; i < numberOfTiles; i++) {
+				int tileType = configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.type");
+				bool isSlope = configFile->Get<bool>("tileset.tiles." + std::to_string(i) + ".tile.slope.is slope");
+				glm::ivec2 slopeOffset = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.slope.slope offset.left"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.slope.slope offset.right"));
+				glm::ivec2 aabbOffset = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.offset.x"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.offset.y"));
+				glm::ivec2 aabbDimensions = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.dimensions.width"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.bounding box.dimensions.height"));
+				glm::ivec2 sourceFramePosition = glm::ivec2(configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.source frame.position.x"), configFile->Get<int>("tileset.tiles." + std::to_string(i) + ".tile.source frame.position.y"));
+				if (indexOfTileSetTexture != -1) {
+					tileList.push_back(Tile(Engine_Pointer->textureRegister[indexOfTileSetTexture], (Tile::Type)tileType, sourceFramePosition, glm::vec3(0.0f), BoundingBox(glm::ivec2(0), aabbDimensions), aabbOffset, isSlope, slopeOffset));
+				}
+				else {
+					tileList.push_back(Tile(Engine_Pointer->textureRegister[indexOfTileSetTexture], (Tile::Type)tileType, sourceFramePosition, glm::vec3(0.0f), BoundingBox(glm::ivec2(0), aabbDimensions), aabbOffset, isSlope, slopeOffset));
+				}
 			}
-			else {
-				tileList.push_back(Tile(Engine_Pointer->textureRegister[indexOfTileSetTexture], (Tile::Type)tileType, sourceFramePosition, glm::vec3(0.0f), BoundingBox(glm::ivec2(0), aabbDimensions), aabbOffset, isSlope, slopeOffset));
-			}
+			return true;
 		}
-		return true;
+		else {
+			Engine_Pointer->engineDebugger.WriteLine(">>>> ERROR: Tileset texture was not found in the texture register");
+			return false;
+		}
 	}
 	else {
 		Engine_Pointer->engineDebugger.WriteLine(">>>> ERROR: Tileset config file wasn't loaded");
