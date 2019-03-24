b130b8c39ef2c53544abd10d48a351f0da4695a3
Tom Reed <t.reed1104@gmail.com>
2019-03-24 00:02:56 +0000

    Commented the linux build script

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+# Create the build directory
 mkdir build
+# Move into the build directory
 cd build
+# Tell conan to build the libraries in our conanfile.txt
 conan install .. --build missing
+# Tell cmake to make our build system
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release     # I think this is right?
+# Tell cmake to actually build the project using our systems preferred compiler
 cmake --build . --config Release
+# Return to the parent directory (our repo head)
 cd ..
