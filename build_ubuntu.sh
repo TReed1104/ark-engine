2b9434f905adbab2a886fd802e7ec4b6d0228359
Tom Reed <t.reed1104@gmail.com>
2019-03-24 00:02:35 +0000

    Added the commands for the experimental linux build script

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+mkdir build
+cd build
+conan install .. --build missing
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release     # I think this is right?
+cmake --build . --config Release
+cd ..
