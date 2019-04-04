00f34771cb30cfa5b82a136b4f28c5582aab1963
Tom Reed <t.reed1104@gmail.com>
2019-04-04 10:12:30 +0100

    added a type cast to prevent the uint32 to float conversion warning

--- src/Background.cpp
+++ src/Background.cpp
@@ -40,7 +40,7 @@ void Background::Draw(void) {
 			glBindTexture(GL_TEXTURE_2D, texture->textureID);
 			glUniform1i(glGetUniformLocation(*shader, "textureSampler"), 0);
 			// TODO: Change to not use SDL_Ticks, due to SDL_Ticks being consistent in its values
-			glUniform1f(glGetUniformLocation(*shader, "time"), SDL_GetTicks());	// Pass SDL_Ticks to the shader as a timer
+			glUniform1f(glGetUniformLocation(*shader, "time"), (float)SDL_GetTicks());	// Pass SDL_Ticks to the shader as a timer
 		}
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, currentMesh.indicesBufferObject);
 		glDrawElements(GL_TRIANGLES, (GLsizei)currentMesh.indices.size(), GL_UNSIGNED_INT, (void*)0);
