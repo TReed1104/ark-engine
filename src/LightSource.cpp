8eec09ce30b662bfeb79eee0c501fb6b96cccbac
Tom Reed <t.reed1104@gmail.com>
2019-05-12 16:41:18 +0100

    Implemented the basics of the LightSource class

--- src/LightSource.cpp
+++ src/LightSource.cpp
@@ -3,9 +3,39 @@
 
 Engine* LightSource::Engine_Pointer;
 
-LightSource::LightSource() {
-
+// Constructors and deconstructors
+LightSource::LightSource(const glm::vec3& lightPosition, const glm::vec3& lightColour) {
+	this->position = lightPosition;
+	this->colour = lightColour;
 }
 LightSource::~LightSource() {
 
+}
+
+// Gets
+const glm::vec3 LightSource::GetPosition(void) {
+	return this->position;
+}
+const glm::vec3 LightSource::GetColour(void) {
+	return this->colour;
+}
+const float LightSource::GetAmbientStrength(void) {
+	return this->ambientStrength;
+}
+const float LightSource::GetSpecularStrength(void) {
+	return this->specularStrength;
+}
+
+// Sets
+void LightSource::SetPosition(const glm::vec3& newPosition) {
+	this->position = newPosition;
+}
+void LightSource::SetColour(const glm::vec3& newColour) {
+	this->colour = newColour;
+}
+void LightSource::SetAmbientStrength(const float& newAmbientStrength) {
+	this->ambientStrength = newAmbientStrength;
+}
+void LightSource::SetSpecularStrength(const float& newSpecularStrength) {
+	this->specularStrength = newSpecularStrength;
 }
--- src/LightSource.h
+++ src/LightSource.h
@@ -8,12 +8,28 @@ class LightSource {
 public:
 	static Engine* Engine_Pointer;
 
-	LightSource();
+	// Constructors and deconstructors
+	LightSource(const glm::vec3& lightPosition = glm::vec3(0.0f), const glm::vec3& lightColour = glm::vec3(1.0f));
 	~LightSource();
 
+	// Gets
+	const glm::vec3 GetPosition(void);
+	const glm::vec3 GetColour(void);
+	const float GetAmbientStrength(void);
+	const float GetSpecularStrength(void);
+
+	// Sets
+	void SetPosition(const glm::vec3& newPosition);
+	void SetColour(const glm::vec3& newColour);
+	void SetAmbientStrength(const float& newAmbientStrength);
+	void SetSpecularStrength(const float& newSpecularStrength);
+
 private:
-
+	// The
+	glm::vec3 position;					// Position of the light in world space
+	glm::vec3 colour;					// Colour of the light
+	float ambientStrength = 0.1f;		// How strong is the light ambience in the world?
+	float specularStrength = 0.5f;		// How strong is the specular spot on a surface?
 };
-
 
 #endif
