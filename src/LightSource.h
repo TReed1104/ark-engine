8031d27d41fef65cbab118719276bc914d2116c4
Tom Reed <t.reed1104@gmail.com>
2019-05-17 11:01:19 +0100

    Re-implemented the Get/Sets for the LightSource header

--- src/LightSource.h
+++ src/LightSource.h
@@ -10,29 +10,54 @@ public:
 	static Engine* Engine_Pointer;
 
 	// Constructors and deconstructors
-	LightSource(const std::string& lightName, const glm::vec3& lightPosition = glm::vec3(0.0f), const glm::vec3& lightColour = glm::vec3(1.0f), const float& lightAmbientStrength = 0.1f, const float& lightSpecularStrength = 0.5f);
+	LightSource(const std::string& lightName, const glm::vec3& lightPosition = glm::vec3(0.0f));
 	~LightSource();
 
 	// Gets
 	const std::string GetName(void);
 	const glm::vec3 GetPosition(void);
-	const glm::vec3 GetColour(void);
-	const float GetAmbientStrength(void);
-	const float GetSpecularStrength(void);
+	const glm::vec3 GetDirection(void);
+	const glm::vec3 GetAmbientIntensity(void);
+	const glm::vec3 GetDiffuseColour(void);
+	const glm::vec3 GetSpecularIntensity(void);
+	const float GetSpotlightCutOff(void);
+	const float GetSpotlightCutOffOuter(void);
+	const float GetAttenuationConstant(void);
+	const float GetAttenuationLinear(void);
+	const float GetAttenuationQuadratic(void);
 
 	// Sets
 	void SetPosition(const glm::vec3& newPosition);
-	void SetColour(const glm::vec3& newColour);
-	void SetAmbientStrength(const float& newAmbientStrength);
-	void SetSpecularStrength(const float& newSpecularStrength);
+	void SetDirection(const glm::vec3& newDirection);
+	void SetAmbientIntensity(const glm::vec3& newAmbientIntensity);
+	void SetDiffuseColour(const glm::vec3& newDiffuseColour);
+	void SetSpecularIntensity(const glm::vec3& newSpecularIntensity);
+	void SetSpotlightCutOff(const float& newCutOff);
+	void SetSpotlightCutOffOuter(const float& newCutOffOuter);
+	void SetAttenuationConstant(const float& newAttenuationConstant);
+	void SetAttenuationLinear(const float& newAttenuationLinear);
+	void SetAttenuationQuadratic(const float& newAttenuationQuadratic);
 
 private:
 	// Attributes of the light source
 	std::string name;
-	glm::vec3 position;					// Position of the light in world space
-	glm::vec3 colour;					// Colour of the light
-	float ambientStrength;				// How strong is the light ambience in the world?
-	float specularStrength;				// How strong is the specular spot on a surface?
+	glm::vec3 position;
+	glm::vec3 direction;
+
+	// Colours and intensities
+	glm::vec3 ambietyIntensity;
+	glm::vec3 diffuseColour;
+	glm::vec3 specularIntensity;
+
+	// Spotlighting
+	float spotlightCutOff;
+	float spotlightCutOffOuter;
+
+	// Attenuation
+	float attenuationConstant;
+	float attenuationLinear;
+	float attenuationQuadratic;
+
 };
 
 #endif
