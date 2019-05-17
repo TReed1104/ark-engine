ba0180d0d21ca35fa39aeb8964f66ebf21fe3140
Tom Reed <t.reed1104@gmail.com>
2019-05-17 12:32:35 +0100

    Rewrote the lightsource class for the new expanded shaders

--- src/LightSource.cpp
+++ src/LightSource.cpp
@@ -3,45 +3,94 @@
 
 Engine* LightSource::Engine_Pointer;
 
-// Constructors and deconstructors
-LightSource::LightSource(const std::string& lightName, const glm::vec3& lightPosition, const glm::vec3& lightColour, const float& lightAmbientStrength, const float& lightSpecularStrength) {
-	this->name = lightName;
-	this->position = lightPosition;
-	this->colour = lightColour;
-	this->ambientStrength = lightAmbientStrength;
-	this->specularStrength = lightSpecularStrength;
+LightSource::LightSource(const std::string& name, const LightingType& type, const glm::vec3& position, const glm::vec3& direction, const glm::vec3& ambientColour, const glm::vec3& diffuseColour, const glm::vec3& specularColour, const float& spotlightCutOff, const float& spotlightCutOffOuter, const float& attenuationConstant, const float& attenuationLinear, const float& attentuationQuadratic) {
+	this->name = name;
+	this->type = type;
+	this->position = position;
+	this->direction = direction;
+	this->ambientColour = ambientColour;
+	this->diffuseColour = diffuseColour;
+	this->specularColour = specularColour;
+	this->spotlightCutOff = spotlightCutOff;
+	this->spotlightCutOffOuter = spotlightCutOffOuter;
+	this->attenuationConstant = attenuationConstant;
+	this->attenuationLinear = attenuationLinear;
+	this->attenuationQuadratic = attentuationQuadratic;
+
+	switch (this->type) {
+		case LightSource::Directional:
+			this->position = glm::vec3(0.0f);
+			break;
+		case LightSource::Point:
+			this->spotlightCutOff = 0.0f;
+			this->spotlightCutOff = 0.0f;
+			break;
+		default:
+			break;
+	}
 }
 LightSource::~LightSource() {
-
 }
 
-// Gets
 const std::string LightSource::GetName(void) {
-	return name;
+	return this->name;
+}
+const LightSource::LightingType LightSource::GetType(void) {
+	return this->type;
 }
 const glm::vec3 LightSource::GetPosition(void) {
 	return this->position;
 }
-const glm::vec3 LightSource::GetColour(void) {
-	return this->colour;
+const glm::vec3 LightSource::GetDirection(void) {
+	return this->direction;
 }
-const float LightSource::GetAmbientStrength(void) {
-	return this->ambientStrength;
+const glm::vec3 LightSource::GetAmbientColour(void) {
+	return this->ambientColour;
 }
-const float LightSource::GetSpecularStrength(void) {
-	return this->specularStrength;
+const glm::vec3 LightSource::GetDiffuseColour(void) {
+	return this->diffuseColour;
 }
-
-// Sets
+const glm::vec3 LightSource::GetSpecularColour(void) {
+	return this->specularColour;
+}
+const float LightSource::GetSpotlightCutOff(void) {
+	return this->spotlightCutOff;
+}
+const float LightSource::GetSpotlightCutOffOuter(void) {
+	return spotlightCutOffOuter;
+}
+const float LightSource::GetAttenuationConstant(void) {
+	return this->attenuationConstant;
+}
+const float LightSource::GetAttenuationQuadratic(void) {
+	return this->attenuationQuadratic;
+}
 void LightSource::SetPosition(const glm::vec3& newPosition) {
 	this->position = newPosition;
 }
-void LightSource::SetColour(const glm::vec3& newColour) {
-	this->colour = newColour;
+void LightSource::SetDirection(const glm::vec3& newDirection) {
+	this->direction = newDirection;
 }
-void LightSource::SetAmbientStrength(const float& newAmbientStrength) {
-	this->ambientStrength = newAmbientStrength;
+void LightSource::SetLightingColours(const glm::vec3& newAmbientColour, const glm::vec3& newDiffuseColour, const glm::vec3& newSpecularColour) {
+	this->ambientColour = newAmbientColour;
+	this->diffuseColour = newDiffuseColour;
+	this->specularColour = newSpecularColour;
 }
-void LightSource::SetSpecularStrength(const float& newSpecularStrength) {
-	this->specularStrength = newSpecularStrength;
+void LightSource::SetAmbientColour(const glm::vec3& newAmbientColour) {
+	this->ambientColour = newAmbientColour;
+}
+void LightSource::SetDiffuseColour(const glm::vec3& newDiffuseColour) {
+	this->diffuseColour = newDiffuseColour;
+}
+void LightSource::SetSpecularColour(const glm::vec3& newSpecularColour) {
+	this->specularColour = newSpecularColour;
+}
+void LightSource::SetSpotlightValues(const float& newCutOff, const float& newCutOffOuter) {
+	this->spotlightCutOff = newCutOff;
+	this->spotlightCutOffOuter = newCutOffOuter;
+}
+void LightSource::SetAttenuation(const float& newConstant, const float& newLinear, const float & newQuadratic) {
+	this->attenuationConstant = newConstant;
+	this->attenuationLinear = newLinear;
+	this->attenuationQuadratic = newQuadratic;
 }
--- src/LightSource.h
+++ src/LightSource.h
@@ -9,45 +9,61 @@ class LightSource {
 public:
 	static Engine* Engine_Pointer;
 
+	enum LightingType { Directional, Point, Spotlight };
+
 	// Constructors and deconstructors
-	LightSource(const std::string& lightName, const glm::vec3& lightPosition = glm::vec3(0.0f));
+	LightSource(const std::string& name, const LightingType& type, const glm::vec3& position = glm::vec3(0.0f), const glm::vec3& direction = glm::vec3(0.0f, 0.0f, 1.0f), 
+				const glm::vec3& ambientColour = glm::vec3(0.2f), const glm::vec3& diffuseColour = glm::vec3(1.0f), const glm::vec3& specularColour = glm::vec3(1.0f),
+				const float& spotlightCutOff = 12.5f, const float& spotlightCutOffOuter = 17.5f, const float& attenuationConstant = 1.0f, const float& attenuationLinear = 0.7f, const float& attentuationQuadratic = 1.8f);
 	~LightSource();
 
-	// Gets
+	// Get Attributes
 	const std::string GetName(void);
+	const LightingType GetType(void);
 	const glm::vec3 GetPosition(void);
 	const glm::vec3 GetDirection(void);
-	const glm::vec3 GetAmbientIntensity(void);
+
+	// Get Light colours and intensities
+	const glm::vec3 GetAmbientColour(void);
 	const glm::vec3 GetDiffuseColour(void);
-	const glm::vec3 GetSpecularIntensity(void);
+	const glm::vec3 GetSpecularColour(void);
+
+	// Get Spotlighting values
 	const float GetSpotlightCutOff(void);
 	const float GetSpotlightCutOffOuter(void);
+
+	// Get Attenuation values
 	const float GetAttenuationConstant(void);
 	const float GetAttenuationLinear(void);
 	const float GetAttenuationQuadratic(void);
 
-	// Sets
+	// Set Attributes
 	void SetPosition(const glm::vec3& newPosition);
 	void SetDirection(const glm::vec3& newDirection);
-	void SetAmbientIntensity(const glm::vec3& newAmbientIntensity);
+
+	// Get Light colours and intensities
+	void SetLightingColours(const glm::vec3& newAmbientColour, const glm::vec3& newDiffuseColour, const glm::vec3& newSpecularColour);
+	void SetAmbientColour(const glm::vec3& newAmbientColour);
 	void SetDiffuseColour(const glm::vec3& newDiffuseColour);
-	void SetSpecularIntensity(const glm::vec3& newSpecularIntensity);
-	void SetSpotlightCutOff(const float& newCutOff);
-	void SetSpotlightCutOffOuter(const float& newCutOffOuter);
-	void SetAttenuationConstant(const float& newAttenuationConstant);
-	void SetAttenuationLinear(const float& newAttenuationLinear);
-	void SetAttenuationQuadratic(const float& newAttenuationQuadratic);
+	void SetSpecularColour(const glm::vec3& newSpecularColour);
+
+	// Get Spotlighting values
+	void SetSpotlightValues(const float& newCutOff, const float& newCutOffOuter);
+
+	// Get Attenuation values
+	void SetAttenuation(const float& newConstant, const float& newLinear, const float& newQuadratic);
 
 private:
 	// Attributes of the light source
 	std::string name;
+	LightingType type;
 	glm::vec3 position;
 	glm::vec3 direction;
 
 	// Colours and intensities
-	glm::vec3 ambietyIntensity;
+	glm::vec3 ambientColour;
 	glm::vec3 diffuseColour;
-	glm::vec3 specularIntensity;
+	glm::vec3 specularColour;
 
 	// Spotlighting
 	float spotlightCutOff;
