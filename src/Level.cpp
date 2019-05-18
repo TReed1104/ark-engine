f41ae53a0c579aa03d95b3fd1e5bf517e0972b64
Tom Reed <t.reed1104@gmail.com>
2019-05-18 11:19:15 +0100

    Added a test Directional light

--- src/Level.cpp
+++ src/Level.cpp
@@ -115,8 +115,8 @@ bool Level::Load(void) {
 		for (size_t i = 0; i < numberOfLightSources; i++) {
 			std::string lightID = configFile->Get<std::string>("level.lightsources." + std::to_string(i) + ".light.id");
 
-			//lightSourcesRegister.push_back(new LightSource(lightID, LightSource::Point, glm::vec3(200.0f, 200.0f, 80.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(1.0f), 12.5f, 17.5f, 1.0f, 0.010f, 0.00005f));	// Test point light
-			lightSourcesRegister.push_back(new LightSource(lightID, LightSource::Point, glm::vec3(200.0f, 200.0f, 80.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(1.0f), 12.5f, 17.5f, 1.0f, 0.010f, 0.00005f));
+			lightSourcesRegister.push_back(new LightSource(lightID, LightSource::Point, glm::vec3(200.0f, 200.0f, 80.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(1.0f), 12.5f, 17.5f, 1.0f, 0.010f, 0.00005f));	// Test point light
+			//lightSourcesRegister.push_back(new LightSource(lightID, LightSource::Directional, glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.2f), glm::vec3(1.0f)));	// Test Directional
 		}
 
 		// Sound effects
