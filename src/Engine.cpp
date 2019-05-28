064395b74460b651f5a6c2521307759d5c6a1600
Tom Reed <t.reed1104@gmail.com>
2019-05-28 09:44:21 +0100

    Reimplemented GetIndexOfTexture()

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1046,7 +1046,7 @@ const int Engine::GetIndexOfTexture(const std::string& textureName) {
 	int indexOfDesiredTexture = -1;
 	const size_t textureRegisterSize = textureRegister.size();
 	for (size_t i = 0; i < textureRegisterSize; i++) {
-		if (textureRegister[i]->GetName().find(textureName) != std::string::npos) {
+		if (textureRegister[i]->GetName() == textureName) {
 			indexOfDesiredTexture = (int)i;
 		}
 	}
