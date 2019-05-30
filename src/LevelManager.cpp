7c78ed369e54bb03c49cc521d91d90a6c28896ac
Tom Reed <t.reed1104@gmail.com>
2019-05-30 10:26:33 +0100

    Fleshed out the LevelManager singleton

--- src/LevelManager.cpp
+++ src/LevelManager.cpp
@@ -1,19 +1,49 @@
 #include "LevelManager.h"
 
-LevelManager* LevelManager::instance = nullptr;
-
-LevelManager::LevelManager() {
-}
-
-LevelManager::~LevelManager() {
-	if (instance != nullptr) {
-		delete instance;
-	}
-}
-
-LevelManager * LevelManager::GetInstance() {
-	if (instance == nullptr) {
-		instance = new LevelManager();
-	}
+LevelManager& LevelManager::GetInstance() {
+	static LevelManager instance;
 	return instance;
+}
+
+void LevelManager::RegisterLevel(Level& newLevel) {
+	levelRegister.push_back(&newLevel);
+}
+const int LevelManager::GetIndexOfLevel(const std::string & levelName) {
+	int indexOfLevel = -1;
+	for (size_t i = 0; i < levelRegister.size(); i++) {
+		if (levelRegister[i]->name == levelName) {
+			indexOfLevel = (int)i;
+		}
+	}
+	return indexOfLevel;
+}
+const Level* LevelManager::GetLevel(const std::string& levelName) {
+	int indexOfLevel = -1;
+	for (size_t i = 0; i < levelRegister.size(); i++) {
+		if (levelRegister[i]->name == levelName) {
+			indexOfLevel = (int)i;;
+		}
+	}
+	if (indexOfLevel == -1) {
+		return nullptr;
+	}
+	return levelRegister[indexOfLevel];
+}
+const Level* LevelManager::GetCurrentLevel(void) {
+	if (indexOfCurrentLevel == -1 || indexOfCurrentLevel > levelRegister.size()) {
+		return nullptr;
+	}
+	else {
+		return levelRegister[indexOfCurrentLevel];
+	}
+}
+bool LevelManager::SetCurrentLevel(const std::string& levelName) {
+	int indexOfNewLevel = GetIndexOfLevel(levelName);
+	if (indexOfNewLevel != -1) {
+		indexOfCurrentLevel = indexOfNewLevel;
+		return true;
+	}
+	else {
+		return false;
+	}
 }
