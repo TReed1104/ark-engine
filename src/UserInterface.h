0493dac71884c073afb888c011e286e3ed700ce8
Tom Reed <t.reed1104@gmail.com>
2019-06-01 00:06:38 +0100

    Further implemention of the UserInterface class declaration

--- src/UserInterface.h
+++ src/UserInterface.h
@@ -1,17 +1,36 @@
 #ifndef ARKENGINE_USERINTERFACE_H_
 #define ARKENGINE_USERINTERFACE_H_
 
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Model.h"
+#include "Texture.h"
+#include "Animation.h"
+#include "BoundingBox.h"
+#include "JsonParser.hpp"
+#include "Font.h"
+#include "TextObject.h"
+
+
 class Engine;
 
 class UserInterface {
 public:
 	static Engine* Engine_Pointer;
 
-	UserInterface();
+	UserInterface(const std::string& name, const std::string& configFile);
 	~UserInterface();
 
-private:
+	const std::string GetName(void);
+	void Update(const float& deltaTime);
+	void Draw(void);
 
+private:
+	JsonFile* configFile = nullptr;
+	std::string name;
 };
 
 #endif
