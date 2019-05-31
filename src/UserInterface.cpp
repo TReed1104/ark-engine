eedee494fdf4f86143e6c175dfbc79deb9db799a
Tom Reed <t.reed1104@gmail.com>
2019-05-31 09:51:12 +0100

    Added the base UserInterface class files

---
+++ src/UserInterface.cpp
@@ -0,0 +1,8 @@
+#include "UserInterface.h"
+
+Engine* UserInterface::Engine_Pointer;
+
+UserInterface::UserInterface() {
+}
+UserInterface::~UserInterface() {
+}
---
+++ src/UserInterface.h
@@ -0,0 +1,17 @@
+#ifndef ARKENGINE_USERINTERFACE_H_
+#define ARKENGINE_USERINTERFACE_H_
+
+class Engine;
+
+class UserInterface {
+public:
+	static Engine* Engine_Pointer;
+
+	UserInterface();
+	~UserInterface();
+
+private:
+
+};
+
+#endif
