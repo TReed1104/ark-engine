bfc3a81e3629ee5f387794d839769b44ba6bfe8a
Tom Reed <t.reed1104@gmail.com>
2019-06-01 00:07:01 +0100

    Added definitions for the expanded UserInterface class

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -2,7 +2,17 @@
 
 Engine* UserInterface::Engine_Pointer;
 
-UserInterface::UserInterface() {
+UserInterface::UserInterface(const std::string& name, const std::string& configFile) {
 }
 UserInterface::~UserInterface() {
+}
+
+const std::string UserInterface::GetName(void) {
+	return this->name;
+}
+void UserInterface::Update(const float& deltaTime) {
+
+}
+void UserInterface::Draw(void) {
+
 }
