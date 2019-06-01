de2bded45fde65d38bf2c1c46c3eb485e198b1ed
Tom Reed <t.reed1104@gmail.com>
2019-06-02 00:41:19 +0100

    Implemented DeleteInterface()

--- src/UserInterfaceManager.cpp
+++ src/UserInterfaceManager.cpp
@@ -9,7 +9,16 @@ void UserInterfaceManager::RegisterInterface(UserInterface& newInterface) {
 	interfaceRegister.push_back(&newInterface);
 }
 void UserInterfaceManager::DeleteInterface(const std::string& interfaceName) {
-
+	// Delete the specified level.
+	const size_t interfaceRegisterSize = interfaceRegister.size();
+	for (size_t i = 0; i < interfaceRegisterSize; i++) {
+		if (interfaceRegister[i]->GetName() == interfaceName) {
+			// This needs double checking?
+			delete interfaceRegister[i];
+			interfaceRegister.erase(interfaceRegister.begin() + i);
+			break;
+		}
+	}
 }
 void UserInterfaceManager::DeleteAllInterfaces(void) {
 	// Delete all the levels.
