4918e3812f3300a4ab58b742deef67cd8ee4b8af
Tom Reed <t.reed1104@gmail.com>
2019-06-02 00:27:35 +0100

    Implemented the DeleteAllInterfaces() function

--- src/UserInterfaceManager.cpp
+++ src/UserInterfaceManager.cpp
@@ -8,6 +8,13 @@ UserInterfaceManager* UserInterfaceManager::GetInstance() {
 void UserInterfaceManager::RegisterInterface(UserInterface& newInterface) {
 	interfaceRegister.push_back(&newInterface);
 }
+void UserInterfaceManager::DeleteAllInterfaces(void) {
+	// Delete all the levels.
+	const size_t interfaceRegisterSize = interfaceRegister.size();
+	for (size_t i = 0; i < interfaceRegisterSize; i++) {
+		delete interfaceRegister[i];
+	}
+}
 bool UserInterfaceManager::Update(const float & deltaTime) {
 	return true;
 }
