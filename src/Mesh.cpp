0f1e65c09e8ee69a35ec3389ce7f0f393ae7b60a
Tom Reed <t.reed1104@gmail.com>
2019-06-13 13:21:24 +0100

    Cleaned up Mesh::BindBuffers()

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -15,38 +15,47 @@ void Model::Mesh::BindBuffers() {
 	glGenVertexArrays(1, &vertexArrayObject);
 	glBindVertexArray(vertexArrayObject);
 
-	if (vertexPositions.size() > 0) {
+	const size_t numberOfVertex = vertexPositions.size();
+	if (numberOfVertex > 0) {
 		glGenBuffers(1, &vertexBufferObject);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-		glBufferData(GL_ARRAY_BUFFER, vertexPositions.size() * sizeof(glm::vec3), &vertexPositions[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, (int)(numberOfVertex) * sizeof(glm::vec3), &vertexPositions[0], GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 	}
-	if (indices.size() > 0) {
+
+	const size_t numberOfIndices = indices.size();
+	if (numberOfIndices > 0) {
 		glGenBuffers(1, &indicesBufferObject);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBufferObject);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, (int)(numberOfIndices) * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 	}
-	if (colourData.size() > 0) {
+
+	const size_t sizeOfColourData = colourData.size();
+	if (sizeOfColourData > 0) {
 		glGenBuffers(1, &colourBufferObject);
 		glBindBuffer(GL_ARRAY_BUFFER, colourBufferObject);
-		glBufferData(GL_ARRAY_BUFFER, colourData.size() * sizeof(glm::vec3), &colourData[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, (int)(sizeOfColourData) * sizeof(glm::vec3), &colourData[0], GL_STATIC_DRAW);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (GLvoid*)0);
 		glEnableVertexAttribArray(1);
 	}
-	if (uvs.size() > 0) {
+
+	const size_t numberOfUVs = uvs.size();
+	if (numberOfUVs > 0) {
 		glGenBuffers(1, &uvBufferObject);
 		glBindBuffer(GL_ARRAY_BUFFER, uvBufferObject);
-		glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, (int)(numberOfUVs) * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (GLvoid*)0);
 		if (isSetupForTextures) {
 			glEnableVertexAttribArray(2);
 		}
 	}
-	if (surfaceNormals.size() > 0) {
+
+	const size_t numberOfSurfaceNormals = surfaceNormals.size();
+	if (numberOfSurfaceNormals > 0) {
 		glGenBuffers(1, &normalsBufferObject);
 		glBindBuffer(GL_ARRAY_BUFFER, normalsBufferObject);
-		glBufferData(GL_ARRAY_BUFFER, surfaceNormals.size() * sizeof(glm::vec3), &surfaceNormals[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, (int)(numberOfSurfaceNormals) * sizeof(glm::vec3), &surfaceNormals[0], GL_STATIC_DRAW);
 		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (GLvoid*)0);
 		glEnableVertexAttribArray(3);
 	}
