8424d0f6e3648f89e2bf204f031f628cda33886f
Tom Reed <t.reed1104@gmail.com>
2019-06-17 00:22:29 +0100

    Implemented the Databinding class functions

---
+++ src/DataBinding.cpp
@@ -0,0 +1,23 @@
+#include "DataBinding.h"
+
+DataBinding::DataBinding(const std::string& name, const BindingTargetType& targetType, const std::string& targetName, const std::string& bindingToken) {
+	this->name = name;
+	this->targetType = targetType;
+	this->targetName = targetName;
+	this->bindingToken = bindingToken;
+}
+DataBinding::~DataBinding() {
+}
+
+const std::string DataBinding::GetName(void) {
+	return this->name;
+}
+const DataBinding::BindingTargetType DataBinding::GetTargetType(void) {
+	return this->targetType;
+}
+const std::string DataBinding::GetTargetName(void) {
+	return this->targetName;
+}
+const std::string DataBinding::GetBindingToken(void) {
+	return this->bindingToken;
+}
