d11859b301ebcb99d980fad831ac86c92bdef700
Tom Reed <t.reed1104@gmail.com>
2019-06-17 00:22:07 +0100

    Reimplemented DataBinding as its own class

---
+++ src/DataBinding.h
@@ -0,0 +1,25 @@
+#ifndef ARKENGINE_DATABINDING_H_
+#define ARKENGINE_DATABINDING_H_
+
+#include <string>
+
+class DataBinding {
+public:
+	enum BindingTargetType { ENGINE, OBJECT, LEVEL };
+	DataBinding(const std::string& name, const BindingTargetType& targetType, const std::string& targetName, const std::string& bindingToken);
+	~DataBinding();
+
+	const std::string GetName(void);
+	const BindingTargetType GetTargetType(void);
+	const std::string GetTargetName(void);
+	const std::string GetBindingToken(void);
+
+private:
+	// Binding settings
+	std::string name = "";
+	BindingTargetType targetType = BindingTargetType::ENGINE;
+	std::string targetName = "";
+	std::string bindingToken = "";
+};
+
+#endif
