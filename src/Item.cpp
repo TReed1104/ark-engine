386659c0bfde83058fa1d551385986f139894413
Tom Reed <t.reed1104@gmail.com>
2019-06-23 00:38:36 +0100

    Added the generic GameObject exports

--- src/Item.cpp
+++ src/Item.cpp
@@ -16,6 +16,24 @@ Item::~Item() {
 std::map<std::string, std::string> Item::ExportDataForBinding(void) {
 	std::map<std::string, std::string> exportData;
 
+	exportData["position"] = std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(position.z);
+	exportData["draw_position"] = std::to_string(drawPosition.x) + ", " + std::to_string(drawPosition.y) + ", " + std::to_string(drawPosition.z);
+	exportData["grid_position"] = std::to_string(gridPosition.x) + ", " + std::to_string(gridPosition.y);
+	if (indexOfShader != -1) {
+		exportData["shader"] = Engine_Pointer->shaderRegister[indexOfShader]->GetName();
+	}
+	else {
+		exportData["shader"] = "Not Set";
+	}
+	exportData["texture"] = texture->GetName();
+	exportData["model"] = model.GetName();
+	if (animationIndex != -1) {
+		exportData["current_animation"] = animations[animationIndex].GetName();
+	}
+	else {
+		exportData["current_animation"] = "Not Set";
+	}
+
 	return exportData;
 }
 void Item::LoadAnimations(void) {
