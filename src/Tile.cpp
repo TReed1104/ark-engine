1a888ea8648bc96967df659f065a929a5f4d04fc
Tom Reed <t.reed1104@gmail.com>
2019-06-23 00:35:42 +0100

    Added the shader, model, texture and animation fields to export

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -76,6 +76,20 @@ std::map<std::string, std::string> Tile::ExportDataForBinding(void) {
 	exportData["position"] = std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(position.z);
 	exportData["draw_position"] = std::to_string(drawPosition.x) + ", " + std::to_string(drawPosition.y) + ", " + std::to_string(drawPosition.z);
 	exportData["grid_position"] = std::to_string(gridPosition.x) + ", " + std::to_string(gridPosition.y);
+	if (indexOfShader != -1) {
+		exportData["shader"] = Engine_Pointer->shaderRegister[indexOfShader]->GetName();
+	}
+	else {
+		exportData["shader"] = "Not Set";
+	}
+	exportData["texture"] = texture->GetName();
+	exportData["model"] = model.GetName();
+	if (animationIndex != -1) {
+		exportData["current_animation"] = animations[animationIndex].GetName();
+	}
+	else {
+		exportData["current_animation"] = "Not Set";
+	}
 
 	return exportData;
 }
